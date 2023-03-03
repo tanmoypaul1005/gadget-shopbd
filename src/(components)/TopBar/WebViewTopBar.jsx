c0e5e7b18436647e04f063d1754eb7893e861022
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-03-03 16:27:38 +0600

    ok

--- src/(components)/TopBar/WebViewTopBar.jsx
+++ src/(components)/TopBar/WebViewTopBar.jsx
@@ -40,8 +40,7 @@ function WebViewTopBar() {
                                     className={`hover:bg-[#2257AA] text-white px-3 py-2 rounded-md text-sm font-medium
                     ${window.location.pathname === '/blog' && 'bg-[#2257AA] text-white'}`}
                                 >
-                                    Blog
-                                </a>
+                                    Blog</a>
 
                                 <a
                                     href="/booking-cart"
@@ -65,7 +64,8 @@ function WebViewTopBar() {
 
                     </div>
 
-                    {!isTopBarOpen && <div className='cursor-pointer flex justify-end items-end invisible md:visible'>
+                    {!isTopBarOpen && <div 
+                    className='cursor-pointer flex justify-end items-end invisible md:visible'>
                         <Image
                             src={iCart}
                             alt="Picture of the author"
